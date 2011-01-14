87e7a99df07ea4b75a3394a37eb8d446089f4bdd
a.novarini <a.novarini@4640d916-c1fb-4d7c-b558-95081f1cb651>
2011-01-14 17:56:36 +0000

    Renamed it directory, written (finally) some decent test for bundle status

---
+++ bundles/jcr/modeshape-server-it/src/test/java/org/apache/sling/jcr/modeshape/server/ModeshapeServerBundleTest.java
@@ -0,0 +1,80 @@
+package org.apache.sling.jcr.modeshape.server;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.ops4j.pax.exam.CoreOptions.felix;
+import static org.ops4j.pax.exam.CoreOptions.mavenConfiguration;
+import static org.ops4j.pax.exam.CoreOptions.options;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.Configuration;
+import org.ops4j.pax.exam.junit.JUnit4TestRunner;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+
+@RunWith( JUnit4TestRunner.class )
+public class ModeshapeServerBundleTest {
+
+    @Configuration
+    public static Option[] configuration() {
+        return options(felix(), mavenConfiguration());
+    }
+
+    @Test
+    public void shouldHaveNotNullBundleContext( BundleContext bundleContext ) throws Exception {
+        assertNotNull(bundleContext);
+
+    }
+
+    @Test
+    public void shouldHaveModeshapeServerBundleLoaded( BundleContext bundleContext ) {
+
+        Bundle[] bundles = bundleContext.getBundles();
+        assertTrue(bundles.length != 0);
+
+        assertTrue(symbolicNamesListFor(bundles).contains("com.sourcesense.stone.jcr.modeshape.server"));
+
+    }
+
+    @Test
+    public void shouldHaveModeshapeServerStarted( BundleContext bundleContext ) throws Exception {
+        Bundle modeshapeServerBundle = getBundle("com.sourcesense.stone.jcr.modeshape.server",bundleContext.getBundles());
+
+        assertEquals(Bundle.ACTIVE, modeshapeServerBundle.getState());
+    }
+
+    private List<String> symbolicNamesListFor( Bundle[] bundles ) {
+    
+        List<String> bundleNames = new ArrayList<String>();
+    
+        for (Bundle bundle : bundles) {
+            bundleNames.add(bundle.getSymbolicName());
+        }
+    
+        return bundleNames;
+    
+        // return Lists.transform(Arrays.asList(bundles), new Function<Bundle, String>() {
+        //
+        // @Override
+        // public String apply( Bundle bundle ) {
+        // return bundle.getSymbolicName();
+        // }
+        // });
+    }
+
+    private Bundle getBundle( String bundleSymbolicName,
+                              Bundle[] bundles ) {
+        
+        for (Bundle bundle : bundles) {
+            if (bundleSymbolicName.equals(bundle.getSymbolicName())) {
+                return bundle;
+            }
+        }
+        
+        return null;
+    }
+}
