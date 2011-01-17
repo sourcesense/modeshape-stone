e9ab6de876ada92384d1ad7c04284f182f821027
a.novarini <a.novarini@4640d916-c1fb-4d7c-b558-95081f1cb651>
2011-01-17 08:43:16 +0000

    Upgraded pax library, refactored integration tests

--- bundles/jcr/modeshape-server-it/src/test/java/org/apache/sling/jcr/modeshape/server/ModeshapeServerBundleTest.java
+++ bundles/jcr/modeshape-server-it/src/test/java/org/apache/sling/jcr/modeshape/server/ModeshapeServerBundleTest.java
@@ -4,9 +4,11 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.ops4j.pax.exam.CoreOptions.felix;
+import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
 import static org.ops4j.pax.exam.CoreOptions.mavenConfiguration;
 import static org.ops4j.pax.exam.CoreOptions.options;
-import java.util.ArrayList;
+import static org.ops4j.pax.exam.CoreOptions.wrappedBundle;
+import java.util.Arrays;
 import java.util.List;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,13 +17,15 @@ import org.ops4j.pax.exam.junit.Configuration;
 import org.ops4j.pax.exam.junit.JUnit4TestRunner;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
+import com.google.common.base.Function;
+import com.google.common.collect.Lists;
 
 @RunWith( JUnit4TestRunner.class )
 public class ModeshapeServerBundleTest {
 
     @Configuration
     public static Option[] configuration() {
-        return options(felix(), mavenConfiguration());
+        return options(felix(), mavenConfiguration(), wrappedBundle(mavenBundle("com.google.collections", "google-collections").version("1.0-rc3")));
     }
 
     @Test
@@ -42,39 +46,31 @@ public class ModeshapeServerBundleTest {
 
     @Test
     public void shouldHaveModeshapeServerStarted( BundleContext bundleContext ) throws Exception {
-        Bundle modeshapeServerBundle = getBundle("com.sourcesense.stone.jcr.modeshape.server",bundleContext.getBundles());
+        Bundle modeshapeServerBundle = getBundle("com.sourcesense.stone.jcr.modeshape.server", bundleContext.getBundles());
 
         assertEquals(Bundle.ACTIVE, modeshapeServerBundle.getState());
     }
 
     private List<String> symbolicNamesListFor( Bundle[] bundles ) {
-    
-        List<String> bundleNames = new ArrayList<String>();
-    
-        for (Bundle bundle : bundles) {
-            bundleNames.add(bundle.getSymbolicName());
-        }
-    
-        return bundleNames;
-    
-        // return Lists.transform(Arrays.asList(bundles), new Function<Bundle, String>() {
-        //
-        // @Override
-        // public String apply( Bundle bundle ) {
-        // return bundle.getSymbolicName();
-        // }
-        // });
+
+        return Lists.transform(Arrays.asList(bundles), new Function<Bundle, String>() {
+
+            @Override
+            public String apply( Bundle bundle ) {
+                return bundle.getSymbolicName();
+            }
+        });
     }
 
     private Bundle getBundle( String bundleSymbolicName,
                               Bundle[] bundles ) {
-        
+
         for (Bundle bundle : bundles) {
             if (bundleSymbolicName.equals(bundle.getSymbolicName())) {
                 return bundle;
             }
         }
-        
+
         return null;
     }
 }
