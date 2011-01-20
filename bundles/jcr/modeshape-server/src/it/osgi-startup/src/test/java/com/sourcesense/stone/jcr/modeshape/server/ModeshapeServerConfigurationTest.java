06e32b6dd8590fc465c093fae984498827d77b2f
a.novarini <a.novarini@4640d916-c1fb-4d7c-b558-95081f1cb651>
2011-01-20 15:10:05 +0000

    Added first integration test about bundle configuration

--- bundles/jcr/modeshape-server/src/it/osgi-startup/src/test/java/com/sourcesense/stone/jcr/modeshape/server/ModeshapeServerConfigurationTest.java
+++ bundles/jcr/modeshape-server/src/it/osgi-startup/src/test/java/com/sourcesense/stone/jcr/modeshape/server/ModeshapeServerConfigurationTest.java
@@ -1,20 +1,33 @@
 package com.sourcesense.stone.jcr.modeshape.server;
 
 import static org.junit.Assert.assertNotNull;
-
-import org.junit.Ignore;
+import static org.ops4j.pax.exam.CoreOptions.felix;
+import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
+import static org.ops4j.pax.exam.CoreOptions.mavenConfiguration;
+import static org.ops4j.pax.exam.CoreOptions.options;
+import static org.ops4j.pax.exam.CoreOptions.wrappedBundle;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.Configuration;
 import org.ops4j.pax.exam.junit.JUnit4TestRunner;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceReference;
 import org.osgi.service.cm.ConfigurationAdmin;
 
+
 @RunWith( JUnit4TestRunner.class )
 public class ModeshapeServerConfigurationTest extends AbstractTestCase {
 
+    @Configuration
+    public static Option[] configuration() {
+        return options(felix(),
+                       mavenConfiguration(),
+                       wrappedBundle(mavenBundle("com.google.collections", "google-collections").version("1.0-rc3")),
+                       mavenBundle("org.ops4j.pax.confman", "pax-confman-propsloader", "0.2.2").startLevel(10));
+    }
+
     @Test
-    @Ignore("We need to load Pax ConfMan first")
     public void shouldHaveConfigAdminServiceRegistered( BundleContext bundleContext ) throws Exception {
 
         final String CONFIG_ADMIN_NAME = ConfigurationAdmin.class.getName();
