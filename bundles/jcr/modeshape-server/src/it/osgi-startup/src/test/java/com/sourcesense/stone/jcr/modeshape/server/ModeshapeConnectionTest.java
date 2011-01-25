3c2f928c035d84b8c47c0ad814725bf4bd2e40d5
l.stancapiano <l.stancapiano@4640d916-c1fb-4d7c-b558-95081f1cb651>
2011-01-25 15:47:42 +0000

    bug fixing....now the other two classes for tests are debuggable

--- bundles/jcr/modeshape-server/src/it/osgi-startup/src/test/java/com/sourcesense/stone/jcr/modeshape/server/ModeshapeConnectionTest.java
+++ bundles/jcr/modeshape-server/src/it/osgi-startup/src/test/java/com/sourcesense/stone/jcr/modeshape/server/ModeshapeConnectionTest.java
@@ -1,35 +1,19 @@
 package com.sourcesense.stone.jcr.modeshape.server;
 
 import static org.junit.Assert.assertNotNull;
-import static org.ops4j.pax.exam.CoreOptions.felix;
-import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
-import static org.ops4j.pax.exam.CoreOptions.mavenConfiguration;
-import static org.ops4j.pax.exam.CoreOptions.options;
-import static org.ops4j.pax.exam.CoreOptions.wrappedBundle;
 
 import java.util.Dictionary;
 import java.util.Hashtable;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.ops4j.pax.exam.Option;
-import org.ops4j.pax.exam.junit.Configuration;
 import org.ops4j.pax.exam.junit.JUnit4TestRunner;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.Constants;
 import org.osgi.framework.ServiceReference;
 
 @RunWith(JUnit4TestRunner.class)
-public class ModeshapeConnectionTest {
-
-	@Configuration
-	public static Option[] configuration() {
-		return options(
-				felix(),
-				mavenConfiguration(),
-				wrappedBundle(mavenBundle("com.google.collections",
-						"google-collections").version("1.0-rc3")));
-	}
+public class ModeshapeConnectionTest extends AbstractTestCase {
 
 	@Test
 	public void shouldTakeTheDefaultWorkspace(BundleContext bundleContext)
--- bundles/jcr/modeshape-server/src/it/osgi-startup/src/test/java/com/sourcesense/stone/jcr/modeshape/server/ModeshapeServerConfigurationTest.java
+++ bundles/jcr/modeshape-server/src/it/osgi-startup/src/test/java/com/sourcesense/stone/jcr/modeshape/server/ModeshapeServerConfigurationTest.java
@@ -1,15 +1,9 @@
 package com.sourcesense.stone.jcr.modeshape.server;
 
 import static org.junit.Assert.assertNotNull;
-import static org.ops4j.pax.exam.CoreOptions.felix;
-import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
-import static org.ops4j.pax.exam.CoreOptions.mavenConfiguration;
-import static org.ops4j.pax.exam.CoreOptions.options;
-import static org.ops4j.pax.exam.CoreOptions.wrappedBundle;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.ops4j.pax.exam.Option;
-import org.ops4j.pax.exam.junit.Configuration;
 import org.ops4j.pax.exam.junit.JUnit4TestRunner;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceReference;
@@ -17,13 +11,6 @@ import org.osgi.service.cm.ConfigurationAdmin;
 
 @RunWith( JUnit4TestRunner.class )
 public class ModeshapeServerConfigurationTest extends AbstractTestCase {
-
-    @Configuration
-    public static Option[] configuration() {
-        return options(felix(),
-                       mavenConfiguration(),
-                       wrappedBundle(mavenBundle("com.google.collections", "google-collections").version("1.0-rc3")));
-    }
 
     @Test
     public void shouldHaveConfigAdminServiceReference( BundleContext bundleContext ) throws Exception {
