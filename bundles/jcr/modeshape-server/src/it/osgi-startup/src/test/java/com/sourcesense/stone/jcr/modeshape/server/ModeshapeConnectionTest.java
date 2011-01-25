4105dadb27844594ed4385790e4ec27080a94d52
l.stancapiano <l.stancapiano@4640d916-c1fb-4d7c-b558-95081f1cb651>
2011-01-25 17:25:12 +0000

    SlingServerRepository is not registered automatically so actually I need to register it manually in my test case

--- bundles/jcr/modeshape-server/src/it/osgi-startup/src/test/java/com/sourcesense/stone/jcr/modeshape/server/ModeshapeConnectionTest.java
+++ bundles/jcr/modeshape-server/src/it/osgi-startup/src/test/java/com/sourcesense/stone/jcr/modeshape/server/ModeshapeConnectionTest.java
@@ -1,7 +1,5 @@
 package com.sourcesense.stone.jcr.modeshape.server;
 
-import static org.junit.Assert.assertNotNull;
-
 import java.util.Dictionary;
 import java.util.Hashtable;
 
@@ -11,6 +9,9 @@ import org.ops4j.pax.exam.junit.JUnit4TestRunner;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.Constants;
 import org.osgi.framework.ServiceReference;
+import static org.junit.Assert.assertNotNull;
+
+import com.sourcesense.stone.jcr.modeshape.server.impl.SlingServerRepository;
 
 @RunWith(JUnit4TestRunner.class)
 public class ModeshapeConnectionTest extends AbstractTestCase {
@@ -19,22 +20,24 @@ public class ModeshapeConnectionTest extends AbstractTestCase {
 	public void shouldTakeTheDefaultWorkspace(BundleContext bundleContext)
 			throws Exception {
 		ServiceReference ssrr = null;
+		BundleContext modeshapeServer = null;
 		try {
+			modeshapeServer = bundleContext.getBundle(2).getBundleContext();
 			Dictionary<String, String> serviceProperties = new Hashtable<String, String>();
 			serviceProperties.put(Constants.SERVICE_VENDOR, "JBoss");
 			serviceProperties.put(Constants.SERVICE_DESCRIPTION, "Test service for SlingRepository");
-			bundleContext.registerService(
-					SlingRepositoryService.class.getName(),
-					new SlingRepositoryService(), serviceProperties);
-			ssrr = bundleContext
-					.getServiceReference(SlingRepositoryService.class.getName());
-			SlingRepositoryService ssr = (SlingRepositoryService) bundleContext
+			modeshapeServer.registerService(
+					SlingServerRepository.class.getName(),
+					new SlingServerRepository(), serviceProperties);
+			ssrr = modeshapeServer
+					.getServiceReference(SlingServerRepository.class.getName());
+			SlingServerRepository ssr = (SlingServerRepository) modeshapeServer
 					.getService(ssrr);
-			//assertNotNull(ssr.getRepository());
+			//assertNotNull(ssr.getDefaultWorkspace());
 		} catch (Exception ex) {
 			throw ex;
 		} finally {
-			bundleContext.ungetService(ssrr);
+			modeshapeServer.ungetService(ssrr);
 		}
 	}
 }
