ae1ae7f97f5abd5e90b44736f3b109f071dc9996
l.stancapiano <l.stancapiano@4640d916-c1fb-4d7c-b558-95081f1cb651>
2011-01-28 18:37:08 +0000

    ok

--- bundles/jcr/modeshape-server/src/it/osgi-startup/src/test/java/com/sourcesense/stone/jcr/modeshape/server/ModeshapeConnectionTest.java
+++ bundles/jcr/modeshape-server/src/it/osgi-startup/src/test/java/com/sourcesense/stone/jcr/modeshape/server/ModeshapeConnectionTest.java
@@ -1,15 +1,15 @@
 package com.sourcesense.stone.jcr.modeshape.server;
 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.ops4j.pax.exam.junit.JUnit4TestRunner;
-import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceReference;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertNotNull;
 
 import com.sourcesense.stone.test.services.EmptyService;
-import com.sourcesense.stone.test.services.SlingServerRepositoryService;
+import com.sourcesense.stone.test.services.SimpleService;
 
 @RunWith(JUnit4TestRunner.class)
 public class ModeshapeConnectionTest extends AbstractTestCase {
@@ -17,16 +17,20 @@ public class ModeshapeConnectionTest extends AbstractTestCase {
 	@Test
 	public void shouldTakeTheDefaultWorkspace()
 			throws Exception {
-		/*
+		
 		ServiceReference serviceReference = bundleContext
-				.getServiceReference(SlingServerRepositoryService.class.getName());
+				.getServiceReference(EmptyService.class.getName());
 
 		assertNull(serviceReference);
 
 		ServiceReference serviceReference2 = bundleContext
-				.getServiceReference(EmptyService.class.getName());
+				.getServiceReference(SimpleService.class.getName());
 
 		assertNotNull(serviceReference2);
-		*/
+		
+		EmptyService ssr = (EmptyService) bundleContext
+				.getService(serviceReference2);
+
+		assertNotNull(ssr);
 	}
 }
