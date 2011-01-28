c86cec1cd68c99cfdccde7df470b0753fadd1f41
l.stancapiano <l.stancapiano@4640d916-c1fb-4d7c-b558-95081f1cb651>
2011-01-28 18:21:59 +0000

    my test

--- bundles/jcr/modeshape-server/src/it/osgi-startup/src/test/java/com/sourcesense/stone/jcr/modeshape/server/ModeshapeConnectionTest.java
+++ bundles/jcr/modeshape-server/src/it/osgi-startup/src/test/java/com/sourcesense/stone/jcr/modeshape/server/ModeshapeConnectionTest.java
@@ -1,37 +1,32 @@
 package com.sourcesense.stone.jcr.modeshape.server;
 
-import static org.junit.Assert.assertNotNull;
-import java.util.Dictionary;
-import java.util.Hashtable;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.ops4j.pax.exam.junit.JUnit4TestRunner;
-import org.osgi.framework.Constants;
+import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceReference;
-import com.sourcesense.stone.jcr.modeshape.server.impl.SlingServerRepository;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertNotNull;
 
-@RunWith( JUnit4TestRunner.class )
+import com.sourcesense.stone.test.services.EmptyService;
+import com.sourcesense.stone.test.services.SlingServerRepositoryService;
+
+@RunWith(JUnit4TestRunner.class)
 public class ModeshapeConnectionTest extends AbstractTestCase {
 
-    @Test
-    public void shouldTakeTheDefaultWorkspace( ) throws Exception {
-        ServiceReference serviceReference = null;
-        try {
-            Dictionary<String, String> serviceProperties = new Hashtable<String, String>();
-            serviceProperties.put(Constants.SERVICE_VENDOR, "JBoss");
-            serviceProperties.put(Constants.SERVICE_DESCRIPTION, "Test service for SlingRepository");
-            bundleContext.registerService(SlingServerRepository.class.getName(), new SlingServerRepository(), serviceProperties);
-            serviceReference = bundleContext.getServiceReference(SlingServerRepository.class.getName());
-            
-            SlingServerRepository ssr = (SlingServerRepository)bundleContext.getService(serviceReference);
-            
-            assertNotNull(ssr);
-        } catch (Exception ex) {
-            throw ex;
-        } finally {
-            if (null != serviceReference) {
-                bundleContext.ungetService(serviceReference);
-            }
-        }
-    }
+	@Test
+	public void shouldTakeTheDefaultWorkspace()
+			throws Exception {
+		/*
+		ServiceReference serviceReference = bundleContext
+				.getServiceReference(SlingServerRepositoryService.class.getName());
+
+		assertNull(serviceReference);
+
+		ServiceReference serviceReference2 = bundleContext
+				.getServiceReference(EmptyService.class.getName());
+
+		assertNotNull(serviceReference2);
+		*/
+	}
 }
