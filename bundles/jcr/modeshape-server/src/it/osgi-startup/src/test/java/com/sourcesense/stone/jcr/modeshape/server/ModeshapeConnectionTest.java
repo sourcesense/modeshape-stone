07da0e233aef0a70cc7945539476e4ef317b8e06
a.novarini <a.novarini@4640d916-c1fb-4d7c-b558-95081f1cb651>
2011-02-16 15:17:52 +0000

    Added test to check if a node could be saved

--- bundles/jcr/modeshape-server/src/it/osgi-startup/src/test/java/com/sourcesense/stone/jcr/modeshape/server/ModeshapeConnectionTest.java
+++ bundles/jcr/modeshape-server/src/it/osgi-startup/src/test/java/com/sourcesense/stone/jcr/modeshape/server/ModeshapeConnectionTest.java
@@ -1,14 +1,19 @@
 package com.sourcesense.stone.jcr.modeshape.server;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+import javax.jcr.Node;
+import javax.jcr.Session;
 import org.apache.sling.jcr.api.SlingRepository;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.modeshape.jcr.api.SecurityContextCredentials;
 import org.ops4j.pax.exam.junit.JUnit4TestRunner;
 import org.osgi.framework.ServiceReference;
 import com.sourcesense.stone.jcr.modeshape.server.impl.SlingServerRepository;
+import com.sourcesense.stone.jcr.modeshape.server.security.CustomSecurityContext;
 import com.sourcesense.stone.test.services.CallingComponentService;
 import com.sourcesense.stone.test.services.SimpleService;
 
@@ -31,10 +36,36 @@ public class ModeshapeConnectionTest extends AbstractTestCase {
     @Test
     public void shouldGetValidSlingRepository() throws Exception {
 
-        ServiceReference slingRepositoryServiceReference = bundleContext.getServiceReference(SlingRepository.class.getName());
-        SlingRepository slingRepository = (SlingRepository)bundleContext.getService(slingRepositoryServiceReference);
+        SlingRepository slingRepository = IntegrationTestUtil.getSlingRepositoryFromServiceList(bundleContext);
 
         assertNotNull(slingRepository);
         assertTrue(slingRepository instanceof SlingServerRepository);        
     }
+    
+    @Test
+    public void shouldSuccessfullyLoginToModeShapeRepository() throws Exception {
+        SlingRepository slingRepository = IntegrationTestUtil.getSlingRepositoryFromServiceList(bundleContext);
+
+        Session session = slingRepository.login(new SecurityContextCredentials(new CustomSecurityContext()));
+        
+        assertNotNull(session);
+    }
+    
+    @Test
+    public void shouldWriteEtcMapNodesIfNotFound() throws Exception {
+        SlingRepository slingRepository = IntegrationTestUtil.getSlingRepositoryFromServiceList(bundleContext);
+        
+        Session session = slingRepository.login(new SecurityContextCredentials(new CustomSecurityContext()));
+        assertFalse(session.nodeExists("/etc/map"));
+        
+        Node root = session.getNode("/");
+        Node etc = root.addNode("etc");
+        etc.addNode("map");
+        
+        session.save();
+        
+        assertTrue(session.nodeExists("/etc/map"));
+        session.logout();
+    }
+
 }
