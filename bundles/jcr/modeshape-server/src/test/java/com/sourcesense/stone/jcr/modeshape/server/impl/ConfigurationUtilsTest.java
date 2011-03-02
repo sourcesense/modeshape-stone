008ea2c5a98879a2a04b693ed4d9bb32d018d469
a.novarini <a.novarini@4640d916-c1fb-4d7c-b558-95081f1cb651>
2011-03-02 15:47:54 +0000

    Removed directories created by tests

--- bundles/jcr/modeshape-server/src/test/java/com/sourcesense/stone/jcr/modeshape/server/impl/ConfigurationUtilsTest.java
+++ bundles/jcr/modeshape-server/src/test/java/com/sourcesense/stone/jcr/modeshape/server/impl/ConfigurationUtilsTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import java.io.File;
+import org.junit.After;
 import org.junit.Test;
 import org.osgi.framework.BundleContext;
 
@@ -11,41 +12,50 @@ public class ConfigurationUtilsTest {
 
     @Test
     public void shouldReturnHomeDirSetUsingPropertySlingRepositoryHome() throws Exception {
-        BundleContext bundleContext = mock(BundleContext.class);
-        when(bundleContext.getProperty(ConfigurationUtils.SLING_REPOSITORY_HOME)).thenReturn("aPath");
-        when(bundleContext.getProperty(ConfigurationUtils.SLING_HOME)).thenReturn(null);
-
+        BundleContext bundleContext = programBundleContextToReturn("aPath", null);
         ConfigurationUtils configurationUtil = new ConfigurationUtils(bundleContext);
 
         File homeDir = configurationUtil.getHomeDir();
 
-        assertEquals("aPath/modeshape",homeDir.getPath());
+        assertEquals("aPath/modeshape", homeDir.getPath());
     }
 
     @Test
     public void shouldReturnHomeDirSetUsingPropertySlingHome() throws Exception {
-        BundleContext bundleContext = mock(BundleContext.class);
-        when(bundleContext.getProperty(ConfigurationUtils.SLING_REPOSITORY_HOME)).thenReturn(null);
-        when(bundleContext.getProperty(ConfigurationUtils.SLING_HOME)).thenReturn("aPath");
-
+        BundleContext bundleContext = programBundleContextToReturn(null, "aPath");
         ConfigurationUtils configurationUtil = new ConfigurationUtils(bundleContext);
 
         File homeDir = configurationUtil.getHomeDir();
 
-        assertEquals("aPath/modeshape",homeDir.getPath());
+        assertEquals("aPath/modeshape", homeDir.getPath());
     }
 
     @Test
     public void shouldReturnRepositoryNameWhenNoPropertySet() throws Exception {
-        BundleContext bundleContext = mock(BundleContext.class);
-        when(bundleContext.getProperty(ConfigurationUtils.SLING_REPOSITORY_HOME)).thenReturn(null);
-        when(bundleContext.getProperty(ConfigurationUtils.SLING_HOME)).thenReturn(null);
-
+        BundleContext bundleContext = programBundleContextToReturn(null, null);
         ConfigurationUtils configurationUtil = new ConfigurationUtils(bundleContext);
 
         File homeDir = configurationUtil.getHomeDir();
 
-        assertEquals("modeshape",homeDir.getPath());
+        assertEquals("modeshape", homeDir.getPath());
     }
 
+    private BundleContext programBundleContextToReturn( String slingRepositoryHome,
+                                               String slingHome ) {
+        BundleContext bundleContext = mock(BundleContext.class);
+        when(bundleContext.getProperty(ConfigurationUtils.SLING_REPOSITORY_HOME)).thenReturn(slingRepositoryHome);
+        when(bundleContext.getProperty(ConfigurationUtils.SLING_HOME)).thenReturn(slingHome);
+        return bundleContext;
+    }
+
+    @After
+    public void tearDown() {
+        deleteResource("aPath/modeshape");
+        deleteResource("aPath");
+    }
+
+    private void deleteResource( String path ) {
+        File aPathDir = new File(path);
+        aPathDir.delete();
+    }
 }
