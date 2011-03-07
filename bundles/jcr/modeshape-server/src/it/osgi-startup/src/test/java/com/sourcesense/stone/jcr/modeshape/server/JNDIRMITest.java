4825062a86fc2ce54bbfee31191e31de747be02c
l.stancapiano <l.stancapiano@4640d916-c1fb-4d7c-b558-95081f1cb651>
2011-03-07 11:37:21 +0000

    jndi test adapted with the new configuration

--- bundles/jcr/modeshape-server/src/it/osgi-startup/src/test/java/com/sourcesense/stone/jcr/modeshape/server/JNDIRMITest.java
+++ bundles/jcr/modeshape-server/src/it/osgi-startup/src/test/java/com/sourcesense/stone/jcr/modeshape/server/JNDIRMITest.java
@@ -1,6 +1,10 @@
 package com.sourcesense.stone.jcr.modeshape.server;
 
+import static com.sourcesense.stone.jcr.modeshape.server.PaxConfigurations.googleCommons;
+import static com.sourcesense.stone.jcr.modeshape.server.PaxConfigurations.slingBasicConfiguration;
+import static com.sourcesense.stone.jcr.modeshape.server.PaxConfigurations.stoneConfiguration;
 import static org.junit.Assert.assertNotNull;
+import static org.ops4j.pax.exam.CoreOptions.options;
 
 import javax.jcr.Repository;
 
@@ -8,11 +12,23 @@ import com.sourcesense.stone.jcr.base.util.RepositoryAccessor;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.Inject;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.Configuration;
 import org.ops4j.pax.exam.junit.JUnit4TestRunner;
+import org.osgi.framework.BundleContext;
 
 @RunWith(JUnit4TestRunner.class)
-public class JNDIRMITest extends AbstractTestCase {
+public class JNDIRMITest  {
 
+    @Inject
+    BundleContext bundleContext;
+    
+    @Configuration
+    public Option[] configuration() {
+        return options(slingBasicConfiguration(), googleCommons(), stoneConfiguration());
+    }
+    
 	@Test
 	public void connectionRetrieved() throws Exception {
 
