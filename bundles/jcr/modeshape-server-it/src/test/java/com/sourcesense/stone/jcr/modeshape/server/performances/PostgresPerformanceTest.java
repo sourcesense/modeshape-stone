e21a3a33c0ccdef255246a2155ee86b1a1a327de
s.tripodi <s.tripodi@4640d916-c1fb-4d7c-b558-95081f1cb651>
2011-03-08 13:22:31 +0000

    plugged different confs depending on backend storage

--- bundles/jcr/modeshape-server-it/src/test/java/com/sourcesense/stone/jcr/modeshape/server/performances/PostgresPerformanceTest.java
+++ bundles/jcr/modeshape-server-it/src/test/java/com/sourcesense/stone/jcr/modeshape/server/performances/PostgresPerformanceTest.java
@@ -1,6 +1,24 @@
 package com.sourcesense.stone.jcr.modeshape.server.performances;
 
+import static com.sourcesense.stone.jcr.modeshape.server.PaxConfigurations.googleCommons;
+import static com.sourcesense.stone.jcr.modeshape.server.PaxConfigurations.slingBasicConfiguration;
+import static com.sourcesense.stone.jcr.modeshape.server.PaxConfigurations.stonePostgresConfiguration;
+import static org.ops4j.pax.exam.CoreOptions.options;
+
+import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.Configuration;
+import org.ops4j.pax.exam.junit.JUnit4TestRunner;
+
+@RunWith(JUnit4TestRunner.class)
 public final class PostgresPerformanceTest extends AbstractPerformanceTest {
+
+    @Configuration
+    public static Option[] configuration() {
+        return options(slingBasicConfiguration(),
+                googleCommons(),
+                stonePostgresConfiguration());
+    }
 
     public PostgresPerformanceTest() {
         super("PostgreSQL");
