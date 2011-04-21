f5d3db47402ca6bc0812ff67c3d480b0df5c50f6
simonetripodi <simone.tripodi@gmail.com>
2011-04-21 08:53:38 +0200

    removed proper stone stuff

--- bundles/jcr/performance-tests/src/test/java/com/sourcesense/stone/jcr/modeshape/server/performances/RemoteJackrabbitPerformanceTest.java
+++
@@ -1,9 +0,0 @@
-package com.sourcesense.stone.jcr.modeshape.server.performances;
-
-public final class RemoteJackrabbitPerformanceTest extends AbstractPerformanceTest {
-
-    public RemoteJackrabbitPerformanceTest() {
-        super("jackrabbit");
-    }
-
-}
